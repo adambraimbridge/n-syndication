33e2ee6109424935db66924a8735262e3f60d942
constantology <christos@muigui.com>
2017-07-10 16:15:33 +0100

    move get-user-status into its own module to make it easier to test

--- src/js/redux.js
+++ src/js/redux.js
@@ -1,17 +1,10 @@
 'use strict';
 
-import { broadcast } from 'n-ui-foundations';
-
-import {
-	FETCH_URI_USER_STATUS,
-	FETCH_OPTIONS_USER_STATUS
-} from './config';
+import getUserStatus from './get-user-status';
 
 import { init as initDataStore } from './data-store';
 import { init as initIconify } from './iconify';
 import { init as initDownloadModal } from './modal-download';
-
-import { cheapClone } from './util';
 
 function init (flags) {
 	if (!flags.get('syndicationRedux')) {
@@ -29,33 +22,4 @@ function init (flags) {
 	});
 }
 
-function getUserStatus () {
-	return fetch(FETCH_URI_USER_STATUS, cheapClone(FETCH_OPTIONS_USER_STATUS))
-		.then(response => {
-			if (response.ok) {
-				return response.json();
-			}
-			else {
-				// this is a valid response, i.e. the user is not a syndication user but somehow managed to get here.
-				// e.g. a developer with the right flags turned on, but doesn't belong to a licence.
-				if (response.status === 401) {
-					return null;
-				}
-
-				return response.text()
-					.then(text => {
-						throw new Error(`Next ${FETCH_URI_USER_STATUS} responded with "${text}" (${response.status})`);
-					});
-			}
-		})
-		.catch(error => {
-			broadcast('oErrors.log', {
-				error: error,
-				info: {
-					component: 'next-syndication-redux'
-				}
-			});
-		});
-}
-
-export { init, getUserStatus };
+export { init };
