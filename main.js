aa2713943270523e9f78493a7c88c6ee000ba8e2
constantology <christos@muigui.com>
2017-08-23 11:38:38 +0100

    export republishing init function and config for use in next-syn-list

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 import { init, checkIfUserIsSyndicationCustomer } from './src/js/index';
+import * as CONFIG from './src/js/config';
+import { init as initRepublishing } from './src/js/republishing';
 
 export {
+	CONFIG,
 	init,
+	initRepublishing,
 	checkIfUserIsSyndicationCustomer
 };
