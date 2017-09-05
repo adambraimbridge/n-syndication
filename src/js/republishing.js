c79ffcb8881481257fc69d6d5c8f856273a05f0b
constantology <christos@muigui.com>
2017-09-05 16:08:50 +0100

    add page tracking  🐿2.5.10

--- src/js/republishing.js
+++ src/js/republishing.js
@@ -1,16 +1,54 @@
 'use strict';
 
+import { broadcast } from 'n-ui-foundations';
+import tracking from 'o-tracking';
+
+import getUserStatus from './get-user-status';
 import { init as initDataStore } from './data-store';
 import { init as initDownloadModal } from './modal-download';
+import { TRACKING } from './config';
 
-function init (flags) {
-	if (typeof window.republishingInitData !== 'function') {
-		window.republishingInitData = function () {};
+function init (flags, user) {
+	if (user) {
+		_init(flags, user);
 	}
-
-	initDataStore(flags, window.republishingInitData());
-
-	initDownloadModal(flags);
+	else {
+		getUserStatus().then(user => {
+			_init(flags, user);
+		});
+	}
 }
 
-export { init };
+function _init (flags, user) {
+	if (!location.pathname.includes('/contract')) {
+		if (typeof window.republishingInitData !== 'function') {
+			window.republishingInitData = function () {};
+		}
+
+		initDataStore(flags, window.republishingInitData());
+
+		initDownloadModal(flags);
+	}
+
+	track(flags, user);
+}
+
+function track (flags, user) {
+	const config = JSON.parse(JSON.stringify(TRACKING.DATA));
+	config.context.contractID = user.contract_id;
+	config.context.appVersion = user.app.version;
+
+	broadcast('oTracking.page', config);
+
+	tracking.init({
+		server: TRACKING.URI
+	});
+
+	tracking.page({
+		app: TRACKING.DATA.context.app,
+		appVersion: user.app.version,
+		contractID: user.contract_id
+	}, () => {});
+}
+
+export { init, track };
