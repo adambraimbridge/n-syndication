36f231877239a507d7166e8ba90cf25069402f83
constantology <christos@muigui.com>
2017-09-08 09:36:44 +0100

    add data-trackable attribute to contractual rights link

--- src/js/config.js
+++ src/js/config.js
@@ -47,9 +47,9 @@ export const LABEL_ARIA_OVERLAY = 'Download: ';
 export const MAX_LOCAL_FORMAT_TIME_MS = 300000;
 
 export const MESSAGES = {
-    MSG_2000: `${EMPTY}<p>Please ensure you have considered your <a href="/republishing/contract">contractual rights</a> before republishing</p>`,
+    MSG_2000: `${EMPTY}<p>Please ensure you have considered your <a data-trackable="contractual-rights" href="/republishing/contract">contractual rights</a> before republishing</p>`,
     MSG_2100: `${EMPTY}<p>This content has already been downloaded and therefore will not count towards your republishing limit.</p>
-<p>Please ensure you have considered your <a href="/republishing/contract">contractual rights</a> before republishing</p>`,
+<p>Please ensure you have considered your <a data-trackable="contractual-rights" href="/republishing/contract">contractual rights</a> before republishing</p>`,
     MSG_2200: `${EMPTY}<p>Please contact us for details of republishing rights for this content (<a href="mailto:syndication@ft.com">syndication@ft.com</a> or +44 (0)207 873 4816).</p>`,
     MSG_2300: `${EMPTY}<p>This content will incur additional charges to republish. Please contact us for further details (<a href="mailto:syndication@ft.com">syndication@ft.com</a> or +44 (0)207 873 4816).</p>`,
     MSG_4000: `${EMPTY}<p>This content is not available for republishing.</p>`,
