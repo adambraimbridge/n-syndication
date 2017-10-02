2259c2bc35ff7ce43d72eada6afa046ed715725f
constantology <christos@muigui.com>
2017-10-02 12:11:30 +0100

    if in maintenance mode, return an object so we can show syndication icons in maintenance mode

--- src/js/get-user-status.js
+++ src/js/get-user-status.js
@@ -18,8 +18,18 @@ export default function getUserStatus () {
 			else {
 				// this is a valid response, i.e. the user is not a syndication user but somehow managed to get here.
 				// e.g. a developer with the right flags turned on, but doesn't belong to a licence.
-				if (response.status === 401) {
-					return null;
+				switch (response.status) {
+					case 401:
+						return null;
+					case 503:
+						if (response.headers.get('content-type').includes('application/json')) {
+							return response.json().then(error => {
+								error.migrated = true;
+								error.MAINTENANCE_MODE = true;
+								
+								return error;
+							});
+						}
 				}
 
 				return response.text()
