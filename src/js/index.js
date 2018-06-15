e8949381d8c307abbb2577baa7385a2079db18e7
Simon Legg <leggsimon@gmail.com>
2018-06-15 23:38:11 +0100

    async/await checkIfUserIsSyndicationCustomer function  🐿 v2.9.0

--- src/js/index.js
+++ src/js/index.js
@@ -2,18 +2,14 @@ import {products as getUserProducts} from 'next-session-client';
 import getUserStatus from './get-user-status';
 import {init as initRedux} from './redux';
 
-const SYNDICATION_PRODUCT_CODE = 'S1';
 
-function checkIfUserIsSyndicationCustomer () {
-	let userIsSyndicationCustomer = false;
-	return getUserProducts()
-		.then(response => {
-			if (response && response.products) {
-				userIsSyndicationCustomer = response.products.includes(SYNDICATION_PRODUCT_CODE);
-			}
-		})
-		.catch(err => err)
-		.then(() => userIsSyndicationCustomer);
+async function checkIfUserIsSyndicationCustomer () {
+	const SYNDICATION_PRODUCT_CODE = 'S1';
+	const response = await getUserProducts().catch(err => err);
+
+	return response && response.products
+		? response.products.includes(SYNDICATION_PRODUCT_CODE)
+		: false;
 }
 
 async function init (flags){
