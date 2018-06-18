79bd58e92ed955a188547bf78960cc10fa926c9c
Simon Legg <leggsimon@gmail.com>
2018-06-18 13:21:18 +0100

    add test placeholders  🐿 v2.9.0

--- test/src/js/index.spec.js
+++ test/src/js/index.spec.js
@@ -4,8 +4,27 @@ const { expect } = require('chai');
 
 const underTest = require('../../../src/js/index');
 
-describe('./src/js/redux', function () {
+describe('#init', function () {
 	it('init should be a Function', function () {
 		expect(underTest.init).to.be.a('function');
 	});
+
+	it.skip('should return undefined if the syndication flag is off and not call stubs', () => { });
+
+	context('when syndication customer', () => {
+		it.skip('should return undefined if the user isn’t migrated', () => { });
+		it.skip('should return initialise the republising navigation', () => { });
+		it.skip('should end if the user isn’t allowed ft.com republishing', () => { });
+		it.skip('should initialise the rest if allowed ft.com republishing', () => { });
+	});
+
+	context('when not syndication customer', () => {
+		it.skip('should not attempt to get user status or initialise syndication', () => { });
+	});
+});
+
+describe('#checkIfUserIsSyndicationCustomer', () => {
+	it.skip('should return true if user has the S1 product code in their products', () => { });
+	it.skip('should return false if user doesn’t have the S1 product code in their products', () => { });
+	it.skip('should return false if there was an error fetching the products', () => { });
 });
