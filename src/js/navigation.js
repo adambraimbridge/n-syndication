6f62b3d97b5b402d3d02d2b9283ed543f56a4b32
Simon Legg <leggsimon@gmail.com>
2018-06-18 16:32:26 +0100

    remove global variable usage  🐿 v2.9.0

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,51 +1,55 @@
 'use strict';
 
 import { $, broadcast } from 'n-ui-foundations';
-//import tracking from 'o-tracking';
-
-let USER_DATA;
 
 function init (user) {
-	insertNavItem('#o-header-nav-desktop', '[data-trackable="My Account"]', '.o-header__nav-link');
+	const insertNavItem = insertUserNavItem(user);
 
-	insertNavItem('#o-header-drawer', '[data-trackable="Portfolio"]', '.o-header__drawer-menu-link');
-
-	USER_DATA = user;
-}
-
-function insertNavItem (selectorContainer, selectorInsertionPoint, selectorLink) {
-	const elCt = $(selectorContainer);
-
-	const elInsertionPoint = elCt.querySelector(selectorInsertionPoint).parentElement;
-
-	const elNavItem = elInsertionPoint.cloneNode(true);
-
-	elNavItem.setAttribute('data-trackable', 'syndication');
-
-	const elNavItemLink = elNavItem.querySelector(selectorLink);
-
-	elNavItemLink.classList.add('n-syndication-republishing');
-	elNavItemLink.setAttribute('data-trackable', 'Republishing');
-	elNavItemLink.setAttribute('href', '/republishing/contract');
-	elNavItemLink.textContent = 'Republishing';
-
-	elInsertionPoint.insertAdjacentElement('afterend', elNavItem);
-
-	elNavItemLink.addEventListener('click', (evt) => {
-		broadcast('oTracking.event', {
-			category: 'syndication',
-			action: 'Republishing',
-			contractID: USER_DATA.contract_id,
-			product: 'syndication',
-			referrer: location.href,
-			url: evt.target.href || evt.target.getAttribute('href')
-		});
+	insertNavItem({
+		selectorContainer: '#o-header-nav-desktop',
+		selectorInsertionPoint: '[data-trackable="My Account"]',
+		selectorLink: '.o-header__nav-link',
 	});
 
-	return { container: elCt, item: elNavItem };
+	insertNavItem({
+		selectorContainer: '#o-header-drawer',
+		selectorInsertionPoint: '[data-trackable="Portfolio"]',
+		selectorLink: '.o-header__drawer-menu-link',
+	});
+}
+
+function insertUserNavItem (user) {
+	return ({ selectorContainer, selectorInsertionPoint, selectorLink }) => {
+		const elCt = $(selectorContainer);
+
+		const elInsertionPoint = elCt.querySelector(selectorInsertionPoint).parentElement;
+
+		const elNavItem = elInsertionPoint.cloneNode(true);
+
+		elNavItem.setAttribute('data-trackable', 'syndication');
+
+		const elNavItemLink = elNavItem.querySelector(selectorLink);
+
+		elNavItemLink.classList.add('n-syndication-republishing');
+		elNavItemLink.setAttribute('data-trackable', 'Republishing');
+		elNavItemLink.setAttribute('href', '/republishing/contract');
+		elNavItemLink.textContent = 'Republishing';
+
+		elInsertionPoint.insertAdjacentElement('afterend', elNavItem);
+
+		elNavItemLink.addEventListener('click', (evt) => {
+			broadcast('oTracking.event', {
+				category: 'syndication',
+				action: 'Republishing',
+				contractID: user.contract_id,
+				product: 'syndication',
+				referrer: location.href,
+				url: evt.target.href || evt.target.getAttribute('href')
+			});
+		});
+	};
 }
 
 export {
-	init,
-	insertNavItem
+	init
 };
